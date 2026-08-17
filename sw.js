c3245cb44a068ea32d978992a07ef3e81d913519
soondet <43376653+soondet@users.noreply.github.com>
2026-08-18 01:38:15 +0500

    content: 45 новых карточек по аудиту, чистка дублей терминов

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: приложение работает офлайн после первой загрузки. */
-const CACHE="jd-c24d7bffdc";
-const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png","basics.js","behav.js","bugs.js","cards-extra.js","cards.js","explainers.js","hb-data.js","sing-data.js","qa-data.js","hacks.js","music-ref.js","guitar.js","books.js","hooks.js","i18n-en.js","more-fp.js","more-term.js","more.js","notes.js","ord.js","pics.js","principles.js","quiz.js","take.js","term-extra.js","terms.js","tier.js","tricky.js","viz-zero.js","zdrill.js","zero.js"];
+const CACHE="jd-c726fb0cdc";
+const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png","basics.js","behav.js","bugs.js","cards-extra.js","cards.js","explainers.js","hb-data.js","sing-data.js","qa-data.js","hacks.js","music-ref.js","guitar.js","books.js","cards-new.js","cards-new-aux.js","hooks.js","i18n-en.js","more-fp.js","more-term.js","more.js","notes.js","ord.js","pics.js","principles.js","quiz.js","take.js","term-extra.js","terms.js","tier.js","tricky.js","viz-zero.js","zdrill.js","zero.js"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS.map(u=>new Request(u,{cache:"no-cache"})))).catch(()=>{}));
