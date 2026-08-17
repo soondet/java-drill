40194121dc8207d89eb3d7283d69f2d605229177
soondet <43376653+soondet@users.noreply.github.com>
2026-08-17 11:53:52 +0500

    fix: обращения звучат одним аккордом, конфетти засыпает, повторный вход в комнату, микрофон после отказа; perf: буферы 3D, sw без двойной закачки

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* Service worker: приложение работает офлайн после первой загрузки. */
-const CACHE="jd-cff66234c4";
+const CACHE="jd-9bbde3d268";
 const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png","basics.js","behav.js","bugs.js","cards-extra.js","cards.js","explainers.js","hb-data.js","sing-data.js","qa-data.js","hacks.js","music-ref.js","hooks.js","i18n-en.js","more-fp.js","more-term.js","more.js","notes.js","ord.js","pics.js","principles.js","quiz.js","take.js","term-extra.js","terms.js","tier.js","tricky.js","viz-zero.js","zdrill.js","zero.js"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS.map(u=>new Request(u,{cache:"reload"})))).catch(()=>{}));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS.map(u=>new Request(u,{cache:"no-cache"})))).catch(()=>{}));
 });
 self.addEventListener("activate",e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
