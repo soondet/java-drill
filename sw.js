9aeec9283facb6e489fe3964e05e7272a5ba5e2b
soondet <43376653+soondet@users.noreply.github.com>
2026-08-17 11:48:56 +0500

    fix: ползунки в 3D, пробел в textarea, утечка сокета комнаты, слушатели и шейдеры 3D, шрифты из сети

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: приложение работает офлайн после первой загрузки. */
-const CACHE="jd-7c4ee4602c";
+const CACHE="jd-eaf19ee436";
 const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png","basics.js","behav.js","bugs.js","cards-extra.js","cards.js","explainers.js","hb-data.js","sing-data.js","qa-data.js","hacks.js","music-ref.js","hooks.js","i18n-en.js","more-fp.js","more-term.js","more.js","notes.js","ord.js","pics.js","principles.js","quiz.js","take.js","term-extra.js","terms.js","tier.js","tricky.js","viz-zero.js","zdrill.js","zero.js"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
